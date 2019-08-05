6d61edb1d31fe5d7b8440a19223825fdc13eeab7
tuda2008 <1756991323@qq.com>
2019-08-05 19:44:36 +0800

    refactor

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -69,7 +69,7 @@ class Api::V1::UsersController < ApplicationController
             if ud
               ud.update_attributes({:author_id => @user.id, :visible => true, :ownership => UserDevice::OWNERSHIP[:user]})
             else
-              UserDevice.create(:author_id => @user.id, :user_id => user.id, :device_id => @device.id, :ownership => UserDevice::OWNERSHIP[:user])
+              UserDevice.create(:author_id => @user.id, :user_id => user.id, :device_id => @device.id, :ownership => UserDevice::OWNERSHIP[:user], :visible => true)
             end
             render json: { status: 1, message: "ok" }
           end 
